1fc02ffde91bab796036ed14c8601b39dacb5910
wxt <602574436@qq.com>
2020-08-25 16:54:42 +0800

    兼容IE

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'react-app-polyfill/ie9';//兼容IE9及之后的版本
 import 'react-app-polyfill/stable';
+import 'core-js/es/map';
+import 'core-js/es/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
